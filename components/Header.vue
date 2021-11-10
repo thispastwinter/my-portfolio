c88714ef86426082c64127b96f5415ab7010606e
Justin Klaas <46500734+thispastwinter@users.noreply.github.com>
2021-11-10 14:54:25 -0600

    playing around with layout

--- components/Header.vue
+++ components/Header.vue
@@ -2,13 +2,13 @@
   <div class="bg-mintCream border-b-2 border-paleSilver">
     <Spacer pl="3" py="3">
       <div class="flex">
-        <div class="p-1" v-for="(letter, index) in firstName" :key="letter">
+        <div class="p-6" v-for="(letter, index) in firstName" :key="letter">
           <Typography :color="colors[index % colors.length]" variant="title1">
             {{ letter }}
           </Typography>
         </div>
         <Spacer px="3"></Spacer>
-        <div class="p-1" v-for="(letter, index) in lastName" :key="letter">
+        <div class="p-6" v-for="(letter, index) in lastName" :key="letter">
           <Typography :color="colors[index % colors.length]" variant="title1">
             {{ letter }}
           </Typography>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,39 @@
 <template>
   <Header />
   <Spacer px="3">
-    <div class="bio-wrapper">
-      <Spacer py="3">
-        <Typography>
-          I'm currently an application developer working at Crema (a product
-          agency) in Kansas City. I'm currently an application developer working
-          at Crema (a product agency) in Kansas City. I'm currently an
-          application developer working at Crema (a product agency) in Kansas
-          City. I'm currently an application developer working at Crema (a
-          product agency) in Kansas City.
-        </Typography>
+      <Spacer pb="3" pt="4">
+        <div class="flex items-center">
+          <img class="rounded-full" width="500em" src="../assets/images/me.jpg" />
+          <Spacer pl="4">
+            <Typography variant="title3">About Me</Typography>
+            <Spacer pt="3">
+              <Typography>
+                A front-end developer specializing in React-Native, GraphQL, and
+                Typescript. I've been developing for nearly 3 years and have a strong
+                passion for learning. I'll never be the best, because this industry is
+                constantly changing, but I'll never get stuck, because I do whatever
+                it takes to get to a solution.
+              </Typography>
+            </Spacer>
+            <Spacer py="3">
+              <Typography>
+                A front-end developer specializing in React-Native, GraphQL, and
+                Typescript. I've been developing for nearly 3 years and have a strong
+                passion for learning. I'll never be the best, because this industry is
+                constantly changing, but I'll never get stuck, because I do whatever
+                it takes to get to a solution.
+              </Typography>
+            </Spacer>
+            <Typography>
+              A front-end developer specializing in React-Native, GraphQL, and
+              Typescript. I've been developing for nearly 3 years and have a strong
+              passion for learning. I'll never be the best, because this industry is
+              constantly changing, but I'll never get stuck, because I do whatever
+              it takes to get to a solution.
+            </Typography>
+          </Spacer>
+        </div>
       </Spacer>
-    </div>
     <Button variant="secondary" @click="onClick" title="I'm a button" />
   </Spacer>
 </template>
@@ -28,5 +49,4 @@ const onClick = () => {
   max-width: 40rem;
   width: "100%";
 }
-
 </style>
